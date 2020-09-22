73154fd87468bfe1921b04d76127d2c1cf7bc9c3
jermaine.brownbasic-fit.com <jermaine.brown@basic-fit.com>
2020-09-22 13:26:03 +0200

    Created server in app.js file, added start script to package json

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
 // CORS is a node.js package for providing a Connect/Express middleware that can be used to enable CORS with various options.
 
+import express from 'express';
+
+// const express = require('express');
+
+const app = express();
+
+const port = 8005;
+
+app.use(express.json());
+
+// TODO: add port in env file 
+app.listen(port, () => {
+  console.log(`Server up and running on ${port}`);
+});
+
+
+
